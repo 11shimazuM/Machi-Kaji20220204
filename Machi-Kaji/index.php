41bea06dd09b1d01acb6774f0766881fa66deedd
11shimazuM <95361235+11shimazuM@users.noreply.github.com>
2022-02-04 17:59:41 +0900

    Add files via upload

---
+++ Machi-Kaji/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Machi-Kaji TOP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" href="styles/suraido.css" >    
+    <link rel="stylesheet" href="styles/reset.min.css" >
+    <link rel="stylesheet" href="styles/style.css" >
+    <link rel="stylesheet" href="styles/header-10.css" >
+  </head>
+
+    <?php include "header.html" ?>
+    
+    <body>
+      <div>
+       <ul class="slider">
+        <li><div class="img"><img src="img/IMG_4695.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4696.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4697.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4698.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4699.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4700.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4889.JPG" alt=""></div></li>
+        <li><div class="img"><img src="img/IMG_4888.JPG" alt=""></div></li>
+       </ul>
+     </div> 
+      <!--/slider-->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="js/header-10.js"></script>
+    <script src="js/suraido.js"></script>
+  </body>
+</html>
---
+++ Machi-Kaji/toiawase.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>お問い合わせ</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/reset.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/header-10.css">
+    <link rel="stylesheet" href="styles/form.css"> 
+</head>
+
+    <?php include "header.html" ?>
+
+    <body>
+<form action="toiawasekakunin.php" method="post" >
+   <br><main class= "label-title">◯　お問い合わせ　◯</main><br><br>
+<!-- ご利用目的 -->
+<div class="mb-3">
+  <label class="form-label">お問い合わせの種類</label><br>
+  <div class="form-check">
+       <label for="mokuteki0"></label>
+       <input type="radio" name="mokuteki0" value="サービス利用について"><label><span class="mgr-20">サービス利用について</span></label>
+       <label for="mokuteki1"></label>
+       <input type="radio" name="mokuteki1" value="サポーターについて"><label><span class="mgr-20">サポーターについて</span></label>
+       <label for="mokuteki2"></label>
+       <input type="radio" name="mokuteki2" value="その他のお問い合わせ"><label><span class="mgr-20">その他のお問い合わせ</span></label>
+    </div>
+</div>
+<br><br>
+<!-- お名前 -->
+<div class="mb-3">
+  <label class="form-label" for="name">お名前</label>
+  <div class="item">
+    <input id="name" class="form-control" type="text" name="name" placeholder="お名前" autocomplete="name" required autofocus /><br>
+  </div>
+</div>
+<!-- Mail -->
+<div class="mb-3">
+    <label class="form-label" for="mail">メールアドレス</label>
+  <div class="item">
+    <input type="text" class="form-control" name="mail" size="30" value="" placeholder="例) machi@kaji.jp" autocomplete="mail" required autofocus /><br>
+  </div>
+</div>
+<!-- LINE ID -->
+<div class="mb-3">
+    <label class="form-label" for="line">LINE ID</label>
+  <div class="item">
+    <input type="text" class="form-control" name="line" size="30" value="" placeholder="連絡手段がLINE希望の場合のみ" /><br>
+  </div>
+</div>
+<!-- 電話番号 -->
+<div class="mb-3">
+  <label class="form-label" for="tel">お電話番号</label>
+  <div class="item">
+    <input type="tel" class="form-control" name="tel" placeholder="例) 09012345678" autocomplete="tel"><br>
+  </div>
+</div>
+<!-- 住所 -->
+<div class="mb-3">
+  <label class="form-label" for="address">住所</label>
+  <div class="item">
+    <input type="text" class="form-control" name="address" size="40" placeholder="例) 北海道札幌市中央区北1条西1丁目1-1-111"><br>
+  </div>
+</div>
+<!-- ご相談内容 -->
+<div class="mb-3">
+  <label class="form-label" for="soudan">ご相談内容</label>
+  <div class="item">
+    <textarea class="form-control" name="soudan" rows="5" placeholder="こちらに内容をご記入ください" required autofocus></textarea><br>
+  </div>
+</div>
+<!-- ボタン -->
+<div class="mb-3">
+  <div class="">
+      <input type="submit" value="入力内容の確認へ">
+      <input type="reset" value="リセット"><br><br><br><br>
+  </div>
+</div>
+</form>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!--自作のJS-->  <!--自作のJS-->
+<script src="js/header-10.js"></script>
+<script>
+  window.addEventListener("beforeunload", function(e) {
+    var confirmationMessage = "入力内容を破棄します。";
+    e.returnValue = confirmationMessage;
+    return confirmationMessage;
+  });
+</script>
+</body>
+</html>
